45d507395a99ac1d8fe8a00519a74308ff2a280f
Mohaan Raja <mohaan.raja@gmail.com>
2025-07-20 10:26:05 +0530

    Testing template

---
+++ RestTraining/src/test/java/org/mohaan/mappers/OrderMapperTest.java
@@ -0,0 +1,26 @@
+package org.mohaan.mappers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderMapperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+
+    @Test
+    void toModel() {
+    }
+}
