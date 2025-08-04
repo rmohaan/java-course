ff2553a828b4995de0c13ab76b3194c8a3a247c0
Mohaan Raja <mohaan.raja@gmail.com>
2025-08-04 22:15:15 +0530

    Custom Queries in JPARepository

--- jpadata/src/main/java/org/mohaan/repositories/AuthorRepository.java
+++ jpadata/src/main/java/org/mohaan/repositories/AuthorRepository.java
@@ -3,5 +3,10 @@ package org.mohaan.repositories;
 import org.mohaan.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    List<Author> findByIdIn(List<Integer> ids);
+    List<Author> findByLastNameContainingIgnoreCase(String lastName);
 }
